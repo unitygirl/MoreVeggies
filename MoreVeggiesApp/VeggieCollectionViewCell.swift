00ef27b3e7ab7be2b40db44813e35f605f2c2eeb
Gangadhar Payyavula <gangadhar.payyavula@gmail.com>
2015-05-14 19:15:16 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    MoreVeggiesApp/Base.lproj/Main.storyboard

---
+++ MoreVeggiesApp/VeggieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  VeggieCollectionViewCell.swift
+//  MoreVeggiesApp
+//
+//  Created by Gangadhar Payyavula on 5/14/15.
+//  Copyright (c) 2015 LaCharite, Noelle. All rights reserved.
+//
+
+import UIKit
+
+class VeggieCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var veggieImae: UIImageView!
+}
