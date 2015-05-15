14ea24a4d070dbbc1c2743702caea9e138d16ac9
unitygirl <noelle.e.lacharite@gmail.com>
2015-05-14 19:02:26 -0700

    facts and label updated
    broccoli fact book updated, label increased to more lines

--- MoreVeggiesApp/BroccoliFactBook.swift
+++ MoreVeggiesApp/BroccoliFactBook.swift
@@ -10,16 +10,16 @@ import Foundation
 
 struct BroccoliFactBook {
     let factsArray = [
-        "Broccoli Fact 1",
-        "Broccoli Fact 2",
-        "Broccoli Fact 3",
-        "Broccoli Fact 4",
-        "Broccoli Fact 5",
-        "Broccoli Fact 6",
-        "Broccoli Fact 7",
-        "Broccoli Fact 8",
-        "Broccoli Fact 9",
-        "Broccoli Fact 10"
+        "Broccoli is very high in Vitamin C, making 1 cup of chopped broccoli the Vitamin C equivalent of an orange.",
+        "Broccoli is high in fiber. Not only in soluble fiber but insoluble as well.",
+        "Broccoli contains the flavonoid kaempferol. Kaempferol is an anti-inflammatory, helps fight against cancer and heart disease.",
+        "Broccoli is one of the very low calorie vegetables; provides just 34 calories per 100 g.",
+        "It contains very good amounts of an anti-oxidant vitamin, vitamin-A, essential for healthy eye-sight.",
+        "With an origin in Italy, broccoli was once known as Italian asparagus.",
+        "The word Broccoli comes from the Italian word ‘brocco’ meaning arm or branch. ",
+        "Broccoli promotes healthy bones.",
+        "Broccoli boosts your immune system.",
+        "Broccoli helps you feel full soyou don't overeat."
     ]
     
     func randomFact() -> String {
